b19e41340c9250757828afd43fa7ef39188d4da7
Кошевой.Евгений <.@TERM1>
2022-11-30 13:47:13 +0300

    Добавьте файлы проекта.

---
+++ Glavnoe.xaml.cs
@@ -0,0 +1,35 @@
+﻿using KoshevoiControl7.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KoshevoiControl7
+{
+    /// <summary>
+    /// Логика взаимодействия для Glavnoe.xaml
+    /// </summary>
+    public partial class Glavnoe : Window
+    {
+        public Glavnoe()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Osnovnoy osnovnoy = new Osnovnoy();
+            osnovnoy.Show();
+            Close();
+        }
+    }
+}
---
+++ View/Windows/Osnovnoy.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KoshevoiControl7.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Osnovnoy.xaml
+    /// </summary>
+    public partial class Osnovnoy : Window
+    {
+        public Osnovnoy()
+        {
+            InitializeComponent();
+        }
+
+        private void PerehodOtchet_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PerehodUchet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PerehodSotrudnik_Click(object sender, RoutedEventArgs e)
+        {
+            Perehodi.Navigate(new DobavlenieSotrudnika());
+
+        }
+
+        private void PerehodTovar_Click(object sender, RoutedEventArgs e)
+        {
+            Perehodi.Navigate(new DobavlenieTovara());
+
+        }
+
+        private void PerehodProizvod_Click(object sender, RoutedEventArgs e)
+        {
+            Perehodi.Navigate(new DobavlenieProizvoditelya());
+        }
+    }
+}
